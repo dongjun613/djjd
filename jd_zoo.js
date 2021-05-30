24bc6622e31fa672c97b698df11faf2ca6679c11
dong.jun <dong.jun@pelerin.cn>
2021-05-30 09:32:34 +0800

    优化

--- jd_zoo.js
+++ jd_zoo.js
@@ -38,9 +38,9 @@ $.inviteList = [];
 $.pkInviteList = [];
 $.secretpInfo = {};
 $.innerPkInviteList = [
-  'sSKNX-MpqKOHu-rvw96HV8bVMTRDUbz-qOdOu4rrjbVoy_M',
-  'sSKNX-MpqKOHu-rvw96HV8bVMTRDUbz-qOdOu4q-0P9H5Bw',
-  'sSKNX-MpqKOJsNu9yM7fBS7fjEa32a7XKiPzEkkNOknfpv3Ea9H9QTKXv0TpCkQ'
+  "sSKNX-MpqKOJsNu9yM7fBS7fjEa32a7XKiPzEkkNOknfpv3Ea9H9QTKXv0TpCkyV",
+  "sSKNX-MpqKOHu-rvw96HV8bVMTRDUbz-qOdOu4q-0P9H5BTr",
+  'sSKNX-MpqKOHu-rvw96HV8bVMTRDUbz-qOdOu4rrjbVoy_u3',
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
