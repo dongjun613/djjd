2d0707e6b26dc6f970aeb1149497e938ce812b2d
dong.jun <dong.jun@pelerin.cn>
2021-07-20 14:30:34 +0800

    update

--- jd_smiek_gua_wealth_island.js
+++ jd_smiek_gua_wealth_island.js
@@ -19,7 +19,7 @@ function randomString(e) {
 }
 $.InviteList = []
 $.innerInviteList = [];
-const HelpAuthorFlag = false;//是否助力作者SH  true 助力，false 不助力
+const HelpAuthorFlag = true;//是否助力作者SH  true 助力，false 不助力
 
 let cookiesArr = [], cookie = '';
 if ($.isNode()) {
@@ -57,8 +57,8 @@ $.appId = 10032;
   let res = [], res2 = [];
   $.InviteLists = []
   if (HelpAuthorFlag) {
-    $.innerInviteList = await getAuthorShareCode('https://raw.githubusercontent.com/smiek2221/updateTeam/master/shareCodes/wealth_island_code_one.json');
-    res2 = await getAuthorShareCode('https://raw.githubusercontent.com/smiek2221/updateTeam/master/shareCodes/wealth_island_code.json');
+    $.innerInviteList = await getAuthorShareCode('http://47.106.222.216:18880/share_code/jxcfd.json');
+    res2 = await getAuthorShareCode('http://47.106.222.216:18880/share_code/jxcfd.json');
     $.innerInviteLists = getRandomArrayElements([...res, ...res2], [...res, ...res2].length);
     $.InviteLists.push(...$.InviteList,...$.innerInviteList,...$.innerInviteLists);
   }else{
