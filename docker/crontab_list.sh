918c1959cced9665d2fea54a539982591b3c70bb
dong.jun <dong.jun@pelerin.cn>
2021-09-10 13:50:03 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -44,7 +44,7 @@
 12 13 * * * node /scripts/jd_shop_sign.js >> /scripts/logs/jd_shop_sign.log 2>&1
 ##############长期活动##############
 # 领卷中心签到
-15 0 * * * node /scripts/jd_ccSign.js >> /scripts/logs/jd_ccSign.log 2>&1
+15 0,12 * * * node /scripts/jd_ccSign.js >> /scripts/logs/jd_ccSign.log 2>&1
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 # 东东超市兑换奖品
