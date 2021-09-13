6baa15ec6dfd259ca9a8334c7ff6c3df1c9b6b7d
dong.jun <dong.jun@pelerin.cn>
2021-09-13 17:41:08 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,6 +10,8 @@
 17 10 * 9,10 * node /scripts/jd_smiek_gua_UnknownTask4.js >> /scripts/logs/jd_smiek_gua_UnknownTask4.log 2>&1
 #内容鉴赏官
 30 10,15 * * * node /scripts/jd_connoisseur.js >> /scripts/logs/jd_connoisseur.log 2>&1
+#开学充电站
+30 1 * * * node /scripts/jd_Ariszy_zy_kxcdz.js >> /scripts/logs/jd_Ariszy_zy_kxcdz.log 2>&1
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 #抢京豆
