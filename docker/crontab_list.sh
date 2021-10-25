6149b1c824d092537a174d0f066dc087b4ae94b9
dong.jun <dong.jun@pelerin.cn>
2021-10-25 14:52:55 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -158,6 +158,7 @@
 10 12 * * * node /scripts/jd_crazy_joy_bonus.js >> /scripts/logs/jd_crazy_joy_bonus.log 2>&1
 #京喜财富岛
 48 0-23/2 * * * node /scripts/jd_smiek_gua_wealth_island.js >> /scripts/logs/jd_smiek_gua_wealth_island.log 2>&1
+48 0-23/3 * * * node /scripts/jd_ls_cfd.js >> /scripts/logs/jd_ls_cfd.log 2>&1
 # 财富大陆互助
 18 0,1,9,14,18 * * * node /scripts/jd_smiek_gua_wealth_island_help.js >> /scripts/logs/jd_smiek_gua_wealth_island_help.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
