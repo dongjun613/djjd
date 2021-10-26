06f80cc5db393f65281b3c16b06ebdfef154e356
dong.jun <dong.jun@pelerin.cn>
2021-10-26 13:53:03 +0800

    update

---
+++ utils/JD_DailyBonus.js
@@ -0,0 +1,11 @@
+{
+    "id": "yc9pro5uyF8CHxYx",
+    "status": "finished",
+    "timestamp": "2021-10-26T05:51:08.189Z",
+    "paths": {
+    "/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js": {
+        "throttled": true,
+            "throttlingReset": 1998
+    }
+}
+}
